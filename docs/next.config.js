65f079e21776edbd161fc4a991ad08ee179d2d56
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-09-25 16:01:14 +0200

    ci: use output export

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,9 +1,7 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
 });
 
 module.exports = withNextra();
