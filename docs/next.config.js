44570d66dd55364b4d760643b2ceb459181d6000
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-09-25 15:27:17 +0200

    ci: use pnpm

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,6 +1,9 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.tsx",
+  images: {
+    unoptimized: true,
+  },
+});
 
-module.exports = withNextra()
+module.exports = withNextra();
