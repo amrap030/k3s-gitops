6ca926b78fe1c42464f79660e37f3ca3793b06dc
Kevin Hertwig <kevin.hertwig@gmail.com>
2023-09-29 01:11:29 +0200

    docs: modify index docs page

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { Icon } from "@iconify/react";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: (
+    <>
+      <Icon icon="logos:kubernetes" height={32} />
+      <span style={{ marginLeft: "12px" }}>amrap030/k3s-gitops</span>
+    </>
+  ),
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/amrap030/k3s-gitops",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/amrap030/k3s-gitops",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Made with ❤️ Kevin Hertwig © 2023",
   },
-}
+};
 
-export default config
+export default config;
